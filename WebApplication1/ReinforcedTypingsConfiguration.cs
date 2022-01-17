ceb43debce777f2aed351500744e726b8c14b87a
Aaron <cooper.a.m@hotmail.com>
2022-01-17 19:10:57 +0000

    .

---
+++ WebApplication1/ReinforcedTypingsConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Reinforced.Typings.Ast.TypeNames;
+using Reinforced.Typings.Fluent;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using ConfigurationBuilder = Reinforced.Typings.Fluent.ConfigurationBuilder;
+
+namespace ReinforcedTypings
+{
+    public static class ReinforcedTypingsConfiguration
+    {
+        public static void Configure(ConfigurationBuilder builder)
+        {
+            // fluent configuration goes here
+
+        }
+    }
+}
